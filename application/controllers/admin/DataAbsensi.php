ae2aeec771ea6f1c643f54daf68241eb17d796f2
bima arif adi perdana <bimamedan@gmail.com>
2020-11-08 20:59:05 +0700

    menampilkan data kehadiran pegawai

---
+++ application/controllers/admin/DataAbsensi.php
@@ -0,0 +1,35 @@
+<?php
+
+class DataAbsensi extends CI_Controller{
+    public function index(){
+        $data['title'] = "Data Absensi Pegawai";
+
+        if((isset($_GET['bulan']) && $_GET['bulan'] != '') && (isset($_GET['tahun']) && $_GET['tahun'] != '')){
+            $bulan = $_GET['bulan'];
+            $tahun = $_GET['tahun'];
+            $bulantahun = $bulan.$tahun;
+        }else{
+            $bulan = date('m');
+            $tahun = date('y');
+            $bulantahun = $bulan.$tahun;
+        }
+
+
+        $data['absensi'] =$this->db->query("SELECT data_kehadiran.*,
+            data_pegawai.nama_pegawai, data_pegawai.jenis_kelamin, data_pegawai.jabatan
+            FROM data_kehadiran
+            INNER JOIN data_pegawai ON data_kehadiran.nik=data_pegawai.nik
+            INNER JOIN data_jabatan ON data_pegawai.jabatan=data_jabatan.nama_jabatan
+            WHERE data_kehadiran.bulan='$bulantahun'
+            ORDER BY data_pegawai.nama_pegawai ASC")->result();
+
+        $this->load->view('templates_admin/header', $data);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/dataAbsensi', $data);
+        $this->load->view('templates_admin/footer');
+    }
+}
+
+
+
+?>
