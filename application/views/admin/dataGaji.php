7625467d3df1c0aa9a574ee819a0f6b411140161
bima arif adi perdana <bimamedan@gmail.com>
2020-11-11 11:50:24 +0700

    menampilkan data gaji

--- application/views/admin/dataGaji.php
+++ application/views/admin/dataGaji.php
@@ -43,8 +43,32 @@
                 <?php } ?>
             </select>
         </div>
-        <button type="submit" class="btn btn-primary mb-2 ml-auto"><i class="fas fa-eye"></i>Tampilkan Data</button>
-        <a href="" class="btn btn-success mb-2 ml-3"><i class="fas fa-plus"></i>Cetak Daftar Gaji</a>
+      <?php
+
+        if((isset($_GET['bulan']) && $_GET['bulan'] != '') && (isset($_GET['tahun']) && $_GET['tahun'] != '')){
+            $bulan = $_GET['bulan'];
+            $tahun = $_GET['tahun'];
+            $bulantahun = $bulan.$tahun;
+        }else{
+            $bulan = date('m');
+            $tahun = date('y');
+            $bulantahun = $bulan.$tahun;
+        }
+
+      ?>
+        <button type="submit" class="btn btn-primary mb-2 ml-auto"><i class="fas fa-eye"></i> Tampilkan Data</button>
+
+        <?php if(count($gaji) > 0) { ?>
+
+          <a href="<?php echo base_url('admin/dataPenggajian/cetakGaji?bulan='.$bulan),'&tahun='.$tahun ?>" class="btn btn-success mb-2 ml-3"><i class="fas fa-print"></i> Cetak Daftar Gaji</a>
+
+        <?php }else{ ?>
+          <!-- Button trigger modal -->
+          <button type="button" class="btn btn-success mb-2 ml-3" data-toggle="modal" data-target="#exampleModal">
+          <i class="fas fa-print"></i> Cetak Daftar Gaji
+          </button>
+        <?php } ?>
+        
     </form>
   </div>
 </div>
@@ -110,4 +134,27 @@
     <span class="badge badge-danger"><i class="fa fas-info-circle"></i>Data absensi masih kosong, silahkan input data kehadiran pada bulan dan tahun yang anda pilih </span>
 <?php  }  ?>
 
+</div>
+
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Informasi</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Data Gaji masih kosong, silahkan input absensi terlebih dahulu pada bulan dan tahun yang anda pilih
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
 </div>
